7b8e41823366403c5c9cc8f66de83997597af7cc
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-10-17 14:42:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 function isSameType(value1, value2) {
   //your js code here
 
-	if(value1 === "NaN"){
-		value1 = NaN;
-	}
-	if(value2 === "NaN"){
-		value2 = NaN;
-	} 
-
 	if(!isNaN(value1) && typeof value1 === "string"){
 		value1 = Number(value1);
 	}
