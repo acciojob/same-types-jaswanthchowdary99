79f307cab162c8b2300d85af47b276545c29af0b
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-10-17 14:39:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function isSameType(value1, value2) {
 	if(isNaN(value1) && isNaN(value2)){
 		return true;
 	}
-	return typeof value1 == typeof value2;
+	return typeof value1 === typeof value2;
 }
 
 // do not change the code below.
